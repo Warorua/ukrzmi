79b1b2dbc4f6f4759d4dce4c46b5ec13efb835e5
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-04-21 18:06:19 +0300

    membership

--- membership/includes/conn.php
+++ membership/includes/conn.php
@@ -5,7 +5,7 @@ Class Database{
 	private $server = "mysql:host=localhost;dbname=tsavosto_news";
 	private $username = "tsavosto_news";
 	private $password = "[VLh_tR&489,";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET UTF8",);
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET cp1251",);
 	protected $conn;
  	
 	public function open(){
