92760a89b66825c794c988e29049105cdd23d082
Warorua <waroruaalex640@gmail.com>
2023-04-19 10:29:40 +0300

    upt 345

--- includes/session.php
+++ includes/session.php
@@ -1,14 +1,11 @@
 <?php
 	include 'includes/conn.php';
-	session_start();
+	//session_start();
 	
 
 date_default_timezone_set("Europe/Kiev");
-	if(isset($_SESSION['admin'])){
-		header('location: admin/home.php');
-	}
-
-date_default_timezone_set("Africa/Nairobi");
+	
+//date_default_timezone_set("Africa/Nairobi");
 
 
 		$conn = $pdo->open();
