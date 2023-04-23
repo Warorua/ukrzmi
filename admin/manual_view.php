89745e05272b8178e5be70faea23bf0d236dee32
Warorua <waroruaalex640@gmail.com>
2023-04-23 20:52:32 +0300

    amin mv

--- admin/manual_view.php
+++ admin/manual_view.php
@@ -81,7 +81,7 @@ include 'components/header.php';
     $type_fin = '';
   }
 
-  $search_where = " WHERE category <> 'international' AND input <> ''
+  $search_where = " WHERE input <> ''
                 " . $type_fin . "
                 " . $tag_1_fin . "
                 " . $tag_2_fin . "
