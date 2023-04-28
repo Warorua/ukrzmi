a0a50c948134a2fc01aa3452f5f47140ddc4fd33
Warorua <waroruaalex640@gmail.com>
2023-04-28 05:24:00 +0300

    panels upt+++

--- admin/lab.php
+++ admin/lab.php
@@ -2,58 +2,16 @@
 include './includes/session.php';
 $conn = $pdo->open();
 
-$tag_1 ='';
-$tag_2= 'alex';
-$tag_3= 'andrew';
-$type = 'antony';
+$wanted_panels = 5;
+$total_panel = $wanted_panels - 1;
+for ($i = 1; $i <= $total_panel; $i++) {
+      $panel_low = $i * 8;
+      if ($i != $total_panel) {
+            $panel_high = 8;
+      } else {
+            $panel_high = 7;
+      }
 
-if ($tag_1 != '') {
-      $tag_1_fin = " AND tag_1 ='" . $tag_1 . "'";
-} else {
-      $tag_1_fin = '';
+      echo $panel_low . ' ' . $panel_high . '<br>';
 }
-///////////////////////////////////////
-if ($tag_2 != '') {
-      $tag_2_fin = " AND tag_2 ='" . $tag_2 . "'";
-} else {
-      $tag_2_fin = '';
-}
-///////////////////////////////////////
-if ($tag_3 != '') {
-      $tag_3_fin = " AND tag_3 ='" . $tag_3 . "'";
-} else {
-      $tag_3_fin = '';
-}
-///////////////////////////////////////
-if ($type != '') {
-      $type_fin = " AND type = '" . $type . "'";
-} else {
-      $type_fin = '';
-}  
-
-//$ser_obj = '{'.cleandt($tag_1_fin).','.cleandt($tag_2_fin).','.cleandt($tag_3_fin).','.cleandt($type_fin).'}';
-
-$ser_obj = '{';
-if($tag_1 != ''){
-      $ser_obj .= cleandt($tag_1_fin).',';
-}
-if($tag_2 != ''){
-      $ser_obj .= cleandt($tag_2_fin).',';
-}
-if($tag_3 != ''){
-      $ser_obj .= cleandt($tag_3_fin).',';
-}
-if($type != ''){
-      $ser_obj .= cleandt($type_fin).',';
-}
-$ser_obj .= '}';
-$ser_obj = str_replace(",}","}", $ser_obj);
-
-//print_r(json_decode($ser_obj, true));
-echo $ser_obj.'<br/>';
-$enc = base64_encode($ser_obj);
-echo $enc.'<br/>';
-$dec = base64_decode($enc);
-echo $dec.'<br/>';
-print_r(json_decode($dec, true));
  ?>
--- desktop/home/panels/panel_05.php
+++ desktop/home/panels/panel_05.php
@@ -1,67 +1,64 @@
-
 <div class="row">
   <?php
 
-$block_news_5 = array_slice($block_news,32,7);
+  $block_news_5 = array_slice($block_news, 32, 7);
 
-foreach($block_news_5 as $row){
-  $catHolder = blockAux($row)['catHolder'];
-  $lastPos = blockAux($row)['lastPos'];
-  $rowtitle = blockAux($row)['rowtitle'];
-  $filtTit = blockAux($row)['filtTit'];
-  $frameColor = blockAux($row)['frameColor'];
-  $titleBadge = blockAux($row)['titleBadge'];
+  foreach ($block_news_5 as $row) {
+    $catHolder = blockAux($row)['catHolder'];
+    $lastPos = blockAux($row)['lastPos'];
+    $rowtitle = blockAux($row)['rowtitle'];
+    $filtTit = blockAux($row)['filtTit'];
+    $frameColor = blockAux($row)['frameColor'];
+    $titleBadge = blockAux($row)['titleBadge'];
 
-      echo articleCard($row, $block, $block_id, $frameColor, $filtTit, $titleBadge, $rowtitle, $catHolder);
-}
+    echo articleCard($row, $block, $block_id, $frameColor, $filtTit, $titleBadge, $rowtitle, $catHolder);
+  }
 
-//for ($x = 0; $x <= 48; $x++) {}
-?>  
-<div class="col-md-3 lastCard<?php echo $block[$block_id]['id']; ?>"> 
-      <div class="card col-sm-4 col-md-3 newsCard" style="background-color:<?php echo $block[$block_id]['bg_color'] ?>">
-    <div class="card-content">
-<div class="card-body d-flex justify-content-center h-75">
+  //for ($x = 0; $x <= 48; $x++) {}
+  ?>
+  <div class="col-md-3 lastCard<?php echo $block[$block_id]['id']; ?>">
+    <div class="card col-sm-4 col-md-3 newsCard" style="background-color:<?php echo $block[$block_id]['bg_color'] ?>">
+      <div class="card-content">
+        <div class="card-body d-flex justify-content-center h-75">
           <div class="m-auto border border-dark rounded p-2 border-2 fw-bold fs-5">
-              <tx class="text-dark text-center">Read all headlines</tx>
+            <tx class="text-dark text-center">Read all headlines</tx>
           </div>
           <a class="stretched-link" href="all_content.php?page=<?php echo $pageName ?>&block_id=<?php echo $block_id ?>&cat_id=<?php echo $block[$block_id]['name'] ?>&cat_type=<?php echo $block[$block_id]['type'] ?>"></a>
+        </div>
+      </div>
     </div>
   </div>
-  </div> 
- </div>
 </div>
 
 <script>
   function isInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
     );
-}
+  }
 
 
-const box<?php echo $block[$block_id]['id']; ?> = document.querySelector('.lastCard<?php echo $block[$block_id]['id']; ?>');
+  const box<?php echo $block[$block_id]['id']; ?> = document.querySelector('.lastCard<?php echo $block[$block_id]['id']; ?>');
 
-setInterval(function(){ 
-  const messageText = isInViewport(box<?php echo $block[$block_id]['id']; ?>) ?
-        'viewed' :
-        'notViewed';
-        
-      if(messageText == 'viewed'){
-       // alert(messageText);
-        document.getElementById('carNext<?php echo $block[$block_id]['id']; ?>').style.visibility = 'hidden';
-      } 
-      else if(messageText == 'notViewed'){
-        document.getElementById('carNext<?php echo $block[$block_id]['id']; ?>').style.visibility = '';
-      } 
+  setInterval(function() {
+    const messageText = isInViewport(box<?php echo $block[$block_id]['id']; ?>) ?
+      'viewed' :
+      'notViewed';
 
-   
-//alert(messageText);
+    if (messageText == 'viewed') {
+      // alert(messageText);
+      document.getElementById('carNext<?php echo $block[$block_id]['id']; ?>').style.visibility = 'hidden';
+    } else if (messageText == 'notViewed') {
+      document.getElementById('carNext<?php echo $block[$block_id]['id']; ?>').style.visibility = '';
+    }
 
-}, 10);
 
+    //alert(messageText);
+
+  }, 10);
 </script>
