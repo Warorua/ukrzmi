4f3fbff869f553f348784c344570586e65640e2e
Warorua <waroruaalex640@gmail.com>
2023-04-28 12:15:16 +0300

    thematic filtration

--- admin/lab.php
+++ admin/lab.php
@@ -1,12 +1,14 @@
 <?php
 include './includes/session.php';
 $conn = $pdo->open();
-
+function posiDescSort($item1,$item2)
+{
+    if ($item1['position'] == $item2['position']) return 0;
+    return ($item1['position'] > $item2['position']) ? 1 : -1;
+}
+$stmt = $conn->prepare("SELECT * FROM blocks WHERE NOT position=:position AND NOT mode=:mode AND page=:page");
+$stmt->execute(['position'=>404, 'mode'=>'', 'page'=>'home']);
+$thematic_block = $stmt->fetchAll();
+usort($thematic_block,'posiDescSort'); 
   
-  
-  
-
-  $image_url = 'https://images.unian.net/photos/2021_03/thumb_files/860_470_1614693804-2518.jpg';
-  $exif_data = generate_exif_data_from_url($image_url);
-  
-  print_r($exif_data);
+  print_r($thematic_block);
