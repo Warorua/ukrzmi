1e057d061797a3de2de6926f0e48f7539d89f97e
Warorua <waroruaalex640@gmail.com>
2023-04-29 23:40:34 +0300

    navbar

--- desktop/includes/navbar.php
+++ desktop/includes/navbar.php
@@ -49,7 +49,7 @@
                     echo '
                      <li class="nav-item dropdown dropdown-mega position-static ' . $navHide . '">
                       <a class="nav-link navLink " href="category.php?cat_id=' . $block[$bb]['type'] . '" data-bs-toggle="" data-bs-auto-close="outside">' . $block[$bb]['name'] . '</a>
-                         ' . dropdown($allNews, $block, $bb) . '
+                         ' . dropdown(newsFetch(), $block, $bb) . '
                      </li>
                      ';
                   }
