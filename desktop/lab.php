8edfab569355007be006907caa27c5d30712f7a6
Warorua <waroruaalex640@gmail.com>
2023-04-29 22:41:02 +0300

    zdfhjf

--- desktop/lab.php
+++ desktop/lab.php
@@ -4,13 +4,14 @@ include 'home/blocks.php';
 
 if (isset($wanted_blocks)) {
     $drp_cnt = $wanted_blocks;
-  } else {
+} else {
     $drp_cnt = 20;
-  }
-  
+}
+$allNews = array_slice(newsFetch(), 0, 5);
 for ($bb = 1; $bb <= $drp_cnt; $bb++) {
     if (isset($block[$bb]['name'])) {
-      echo dropdown(newsFetch(), $block, $bb);
+        //echo dropdown(newsFetch(), $block, $bb);
+        echo ukrzmiCard($row, $block, $block_id);
     }
-  }
+}
 
