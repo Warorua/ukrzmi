09a6fe548a0a27a3b344277b7e02bded908e926c
Warorua <waroruaalex640@gmail.com>
2023-04-29 22:37:30 +0300

    srtjsr

--- desktop/lab.php
+++ desktop/lab.php
@@ -2,6 +2,12 @@
 include './includes/session.php';
 include 'home/blocks.php';
 
+if (isset($wanted_blocks)) {
+    $drp_cnt = $wanted_blocks;
+  } else {
+    $drp_cnt = 20;
+  }
+  
 for ($bb = 1; $bb <= $drp_cnt; $bb++) {
     if (isset($block[$bb]['name'])) {
       echo dropdown(newsFetch(), $block, $bb);
