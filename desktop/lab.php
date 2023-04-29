2927ba8ea83cde06bd8628e3cca06433f641ae1c
Warorua <waroruaalex640@gmail.com>
2023-04-29 22:46:46 +0300

    sthjsrthj

--- desktop/lab.php
+++ desktop/lab.php
@@ -11,7 +11,7 @@ $allNews = array_slice(newsFetch(), 0, 5);
 for ($bb = 1; $bb <= $drp_cnt; $bb++) {
     if (isset($block[$bb]['name'])) {
         //echo dropdown(newsFetch(), $block, $bb);
-        echo ukrzmiCard($row, $block, $block_id);
+        echo ukrzmiCard($newsFetch(), $block, $bb);
     }
 }
 
