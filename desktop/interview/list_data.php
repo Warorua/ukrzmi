ee75b6f5d6a5cd382cf1d10762a962bb2ba000d6
Warorua <waroruaalex640@gmail.com>
2023-04-30 23:28:54 +0300

    fgmjgfmh

--- desktop/interview/list_data.php
+++ desktop/interview/list_data.php
@@ -1,6 +1,6 @@
 <?php
 include('../full_coverage/vendor/autoload.php');
-include '../includes/conn.php';
+include '../includes/conn_2.php';
 include '../includes/functions.php';
 
 use \NlpTools\Tokenizers\WhitespaceTokenizer;
